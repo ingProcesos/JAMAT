5c296a3d7e1421811ab47d64d36148ab68404409
ingProcesos <ing.procesos.jamat@gmail.com>
2025-09-01 12:28:18 -0600

    Add files via upload

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,11 @@
+# __manifest__
+{
+    "name": "Real Estate",
+    "version": "18.0.1.0.0",
+    "depends": ["base"],
+    "author": "Author Name",
+    "category": "Custom/Training",
+    "description": """
+        Description text
+    """
+}
