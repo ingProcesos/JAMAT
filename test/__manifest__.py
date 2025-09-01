c4dd8531420abb46193be8a3d5b886e05e702110
ivgm-odoo <139793899+ivgm-odoo@users.noreply.github.com>
2025-09-01 11:17:43 -0600

    New test module

---
+++ test/__manifest__.py
@@ -0,0 +1,15 @@
+# __manifest__
+{
+    "name": "New Module",
+    "version": "18.0.1.0.0",
+    "depends": ["base"],
+    "author": "Author Name",
+    "category": "Custom/Training",
+    "description": """
+        Description text
+    """,
+    # "data": [
+    #     "views/mymodule_view.xml",
+    #     "security/mymodule_security.csv"
+    # ],
+}
