03b56d43f9bf4035216d0ce8833dd24a62925043
ingProcesos <ing.procesos.jamat@gmail.com>
2025-09-02 10:54:07 -0600

    test

--- real_estate/models/estate_property_type.py
+++ real_estate/models/estate_property_type.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 class EstatePropertyType(models.Model):
-    _name = "estate_property_type"
+    _name = "estate.property.type"
     _description = "Property Type"
 
     name = fields.Char(string="Property Type", required=True)
