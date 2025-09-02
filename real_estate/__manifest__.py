484168e0e6fb834f25de905df0e5926b337d985f
ingProcesos <ing.procesos.jamat@gmail.com>
2025-09-02 08:56:42 -0600

    test

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -12,6 +12,7 @@
         "views/estate_property_views.xml",
         "views/estate_property_list.xml",
         "views/estate_property_form.xml",
+        "views/estate_property_search.xml",
         "security/ir.model.access.csv"
     ],
 }
